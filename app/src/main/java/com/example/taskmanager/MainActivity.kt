cfc536ec4bf362bd2e9606bab1cedf2cb64214fc
Danila <danilka.eremin@gmail.com>
2022-06-07 16:41:50 +0800

    first

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+
+
+            TaskManagerTheme {
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TaskManagerTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/taskmanager/tool/CardTask.kt
@@ -0,0 +1,159 @@
+package com.example.taskmanager.tool
+
+import android.widget.ToggleButton
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmanager.R
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun CardTask() {
+    val backgroundColor = Color.Yellow
+    val listTag = listOf("School", "Everyday")
+    val label = "Пойти в школу"
+    val date = "7 октября 2022"
+    val time = "17:00"
+    Surface(
+        color = backgroundColor
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Row() {
+                    listTag.forEach {
+                            tag ->
+                        Chip(
+                            onClick = {  },
+                            border = BorderStroke(1.dp, Color.Gray),
+                            colors = ChipDefaults.chipColors(backgroundColor = backgroundColor),
+                            modifier = Modifier.padding(end = 10.dp)
+                        ) {
+                            Text(
+                                text = tag,
+                                fontFamily = FontFamily.Monospace
+                            )
+                        }
+                    }
+                }
+
+                IconButton(onClick = {  }) {
+                    Icon(imageVector = Icons.Default.Settings, contentDescription = "")
+                }
+            }
+            
+
+            
+            Text(
+                text = label,
+                fontWeight = FontWeight.Bold,
+                fontSize = 26.sp,
+                modifier = Modifier.padding(vertical = 10.dp)
+            )
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_baseline_calendar_month_24),
+                    contentDescription = "",
+                    tint = Color.Black,
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+
+                Text(text = date)
+            }
+            
+            Spacer(modifier = Modifier.size(8.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_baseline_access_time_24),
+                        contentDescription = "",
+                        tint = Color.Black,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                    Text(text = time)
+                }
+
+
+                Done(){
+
+                }
+
+            }
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun Done(
+    onDone: () -> Unit
+) {
+    val check = remember{ mutableStateOf(false) }
+    val tint by animateColorAsState(
+        if (check.value) Color(0xFF000000) else Color.Transparent
+    )
+
+    Surface(
+        onClick = {
+            check.value = !check.value
+            onDone()
+        },
+        modifier = Modifier
+            .size(30.dp),
+        border = BorderStroke(1.dp, Color.Black),
+        color = tint,
+        shape = CircleShape
+    ) {
+
+    }
+
+}
+
+
+@Preview
+@Composable
+fun PreviewCardTask() {
+    TaskManagerTheme {
+        CardTask()
+    }
+}
